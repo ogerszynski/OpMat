76f30c7b2d5ee1a56edee1684a67df24ccec6913
ogerszynski <151456125+ogerszynski@users.noreply.github.com>
2024-04-29 23:01:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main() //glówna metoda
+    {
+        Console.WriteLine("Podaj dodatnią liczbę całkowitą:");
+        int number = int.Parse(Console.ReadLine());
+
+        bool result = OpMat (number);
+        Console.WriteLine("Czy liczba spełnia warunek? " + result);
+    }
+
+    static bool OpMat (int n) //def metody
+    {
+        int sum = 0;
+        int temp = n;
+        int numDigits = (int)Math.Floor(Math.Log10(n)) + 1; // liczba cyfr w n
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += (int)Math.Pow(digit, numDigits);
+            temp /= 10;
+        }
+
+        // zwraca sprawdzojy wynik
+        return sum == n;
+    }
+}
